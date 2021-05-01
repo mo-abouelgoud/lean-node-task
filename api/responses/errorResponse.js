a25492103ed22b2405cccd59a36333c502869285
approckw2013 <approckw@gmail.com>
2021-05-01 05:09:16 +0200

    create custom response to track error & success responses

---
+++ api/responses/errorResponse.js
@@ -0,0 +1,26 @@
+module.exports = function(status,message,data) {
+
+  var req = this.req;
+  var res = this.res;
+
+
+  var statusCode = status;
+
+  var result = {
+    status: statusCode
+  };
+
+  // Optional message
+  if (message) {
+    result.message = message;
+  }
+
+    // Optional data
+    if (data) {
+        //always return data variable as an object
+        result.data = _.isString(data) ? {data} : data;
+  }
+    
+    return res.json(result);
+
+}
