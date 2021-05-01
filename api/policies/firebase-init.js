9cd80c49791bd91d34ede4eeef11c94ca33c4af4
approckw2013 <approckw@gmail.com>
2021-05-01 05:17:11 +0200

    init firebase data

---
+++ api/policies/firebase-init.js
@@ -0,0 +1,12 @@
+
+  const firebase = require('firebase-admin');
+ 
+firebase.initializeApp({
+  credential: firebase.credential.cert(sails.config.custom.firebase_config),
+  databaseURL:sails.config.custom.firebase_config.url
+});
+
+module.exports = function (req, res, next) {
+ 
+  next();
+};
