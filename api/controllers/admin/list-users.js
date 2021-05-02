a722d723d8f3da1ab8f275469162ea556bfcb00f
approckw2013 <approckw@gmail.com>
2021-05-02 07:45:42 +0200

    refactor list users to get data from algolia paginated

--- api/controllers/admin/list-users.js
+++ api/controllers/admin/list-users.js
@@ -1,12 +1,26 @@
 
-
+var algoliasearch = require("algoliasearch");
 
 module.exports = {
 	friendlyName: 'get normal user details',
 	description: 'get normal user details',
 
 	inputs: {
-		
+			username: {
+			type: 'string',
+			defaultsTo: ""
+        },
+        
+		email: {
+			type: 'string',
+			defaultsTo: ""
+		},
+		page: {
+            type: 'number',
+		},
+		limit: {
+            type: 'number',
+		},
 	},
 
 	exits: {
@@ -27,22 +41,46 @@ module.exports = {
 
     fn: async function (inputs, exits) {
        
-		 let user = this.req.user
+		//  let user = this.req.user
 		
-		if (!user)
-			return this.res.errorResponse(sails.config.custom.responseCodes.notFound,
-                sails.__('user_not_found'))
+		// if (!user)
+		// 	return this.res.errorResponse(sails.config.custom.responseCodes.notFound,
+        //         sails.__('user_not_found'))
         
-		if(_.isArray(user))
-            user = _.last(user);
+		// if(_.isArray(user))
+        //     user = _.last(user);
         
-        //delete the password and id from the user obj
-        user= _.omit(user, [
-	    		'password','id'
-	    	])
- 		 
-         return this.res.successResponse(sails.config.custom.responseCodes.success
-            , sails.__('mission_success'), {user})			 
+        // //delete the password and id from the user obj
+        // user= _.omit(user, [
+	    // 		'password','id'
+	    // 	])
+
+		const client = algoliasearch(sails.config.algolia_config.api_id , sails.config.algolia_config.admin_api_key);
+    	const index = client.initIndex(sails.config.algolia_config.index_name);
+
+		let filters = '';
+		if (inputs.username)
+			filters = filters + `username:${inputs.username}`
+		if (inputs.email)
+			filters = filters + ( filters ? ' OR ' : '') + `email:${inputs.email}`
+		
+		console.log("filters",filters)
+		
+		index.search('', {
+			page: inputs.page,
+			hitsPerPage: inputs.limit,
+			 filters: filters
+			}).then(({ hits }) => {
+			// console.log("results",hits);
+			 
+			return this.res.successResponse(sails.config.custom.responseCodes.success
+				, sails.__('mission_success'), hits);
+			
+		}).catch((error) => {
+			return this.res.errorResponse(sails.config.custom.responseCodes.serverError,
+				sails.__('server_error'), { error: error });
+		 });
+ 				 
 		 
 	}
 }
